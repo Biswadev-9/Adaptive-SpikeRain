05dc8b17154f529e7828a63f9c7328f15f569103
Biswadev-9 <dev.biswa01@gmail.com>
2026-09-06 02:02:05 +0600

    Implement Rain100L dataset loader

---
+++ data/dataset.py
@@ -0,0 +1,54 @@
+import os
+from PIL import Image
+
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+
+class Rain100LDataset(Dataset):
+    """
+    Dataset loader for Rain100L image deraining dataset.
+
+    input  folder : rainy images
+    target folder : clean images
+    """
+
+    def __init__(self, root_dir, transform=None):
+
+        self.input_dir = os.path.join(root_dir, "input")
+        self.target_dir = os.path.join(root_dir, "target")
+
+        self.images = sorted(os.listdir(self.input_dir))
+
+        self.transform = transform
+
+
+    def __len__(self):
+        return len(self.images)
+
+
+    def __getitem__(self, idx):
+
+        img_name = self.images[idx]
+
+        rainy_path = os.path.join(
+            self.input_dir,
+            img_name
+        )
+
+        clean_path = os.path.join(
+            self.target_dir,
+            img_name
+        )
+
+
+        rainy = Image.open(rainy_path).convert("RGB")
+        clean = Image.open(clean_path).convert("RGB")
+
+
+        if self.transform:
+            rainy = self.transform(rainy)
+            clean = self.transform(clean)
+
+
+        return rainy, clean
